1d0b4f9bd40d1377d23a57f7359c48d81a655419
vygL <valleylin410@gmail.com>
2025-03-31 20:43:42 -0700

    Window loop

--- src/main.c
+++ src/main.c
@@ -7,12 +7,17 @@ void error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error: %s", description);
 }
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (!glfwInit()) {
         fprintf(stderr, "%s", "Failed to Initialized GLFW");
     }
     glfwSetErrorCallback(error_callback);
-
     const char* pName = "AeroSim";
     int wWidth = 800;
     int wHeight = 600;
@@ -20,9 +25,12 @@ int main(int argc, char* argv[]) {
     if (!window) {
         fprintf(stderr, "%s", "Failed to Initialized Window");
     }
+    glfwSetKeyCallback(window, key_callback);
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    
+    while (!glfwWindowShouldClose(window)) {
+
+    }
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
