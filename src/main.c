4665f45b3b115e7de382ca659d6b0cf1b9aa372d
vygL <valleylin410@gmail.com>
2025-03-31 19:48:37 -0700

    GLFW setup

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[]) {
+#include <stdio.h>
 
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "GLFW Error: %s", description);
+}
+
+int main(int argc, char* argv[]) {
+    if (!glfwInit()) {
+        fprintf(stderr, "%s", "Failed to Initialized GLFW");
+    }
+    glfwSetErrorCallback(error_callback);
+    glfwTerminate();
     return 0;
 }
